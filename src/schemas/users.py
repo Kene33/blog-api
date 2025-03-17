d512e94844cd6cd52e41d07ba8268e1ce3d70b48
Kene33 <hamid.hokage2009@gmail.com>
2025-03-18 00:08:04 +0300

    add profile

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,11 +1,4 @@
-from pydantic import BaseModel, Field
-
-class User(BaseModel):
-    id: int
-    name: str = Field(title="Имя")
-    reg_date: str = Field(title="Дата регистрации")
-    posts_quantity: int = Field(title="Количество постов")
-
+from pydantic import BaseModel
 
 class UserLoginSchema(BaseModel):
     username: str
